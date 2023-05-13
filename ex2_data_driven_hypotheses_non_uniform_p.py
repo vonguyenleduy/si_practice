ac29df47f3c40694a4fb37ae9996973c06c0935f
Vo Nguyen Le Duy <duylenguyen.vo@gmail.com>
2023-05-13 13:47:55 +0900

    Update

---
+++ ex2_data_driven_hypotheses_non_uniform_p.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+
+
+def run():
+    n = 3
+    x = np.random.normal(loc=0, scale=1, size=(n, 1))
+
+    # vector of direction of interest (testing the maximum element)
+    idx = np.argmax(x)
+    eta = np.zeros((n, 1))
+    eta[idx] = 1
+
+    # test-statistic
+    etaTx = np.dot(eta.T, x)[0][0]
+
+    # compute p_value
+    p_value = 1 - norm.cdf(etaTx)
+
+    return p_value
+
+
+if __name__ == '__main__':
+    # np.random.seed(5)
+    # run()
+
+    max_iteration = 12000
+    list_p_value = []
+
+    for each_iter in range(max_iteration):
+        if each_iter % 10 == 0:
+            print(each_iter)
+
+        p_value = run()
+
+        if p_value is not None:
+            list_p_value.append(p_value)
+
+    plt.rcParams.update({'font.size': 17})
+    plt.hist(list_p_value)
+    plt.tight_layout()
+    plt.show()
